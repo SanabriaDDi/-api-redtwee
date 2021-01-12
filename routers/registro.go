774fc01c317a0d67a4842ba974c48113cb515f79
Diego <diegosanabriadacosta@gmail.com>
2021-01-12 00:50:41 -0600

    Corrige validaciones de registro
    Añade return's a condicionales if en caso de fallar una validacion. El
    no hacerlo implica que la función continue ejecutandose causando
    duplicidad de registros.

--- routers/registro.go
+++ routers/registro.go
@@ -20,23 +20,28 @@ func Registro(w http.ResponseWriter, r *http.Request)  {
 
 	if len(t.Email) == 0 {
 		http.Error(w, "El email de usuario es requerido", 400)
+		return
 	}
 	if len(t.Password) < 6 {
 		http.Error(w, "Debe especificar una contraseña de al menos 6 caracteres", 400)
+		return
 	}
 
 	_, encontrado, _ := bd.ChequeoYaExisteUsuario(t.Email)
 	if encontrado == true {
 		http.Error(w, "Ya esxiste un usuario registrado con ese email", 400)
+		return
 	}
 
 	_, status, err := bd.InsertoRegistro(t)
 	if err != nil {
 		http.Error(w, "Ocurrió un error al intentar realizar el registro de usuario "+err.Error(), 400)
+		return
 	}
 
 	if status == false {
 		http.Error(w, "No se ha logrado insertar el registro del usuario", 400)
+		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
